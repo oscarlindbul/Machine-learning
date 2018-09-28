4647adb78bb8a2fc0fa9cc001205f3094a8df7db
Kouhai <bogus Email>
2018-09-28 13:18:45 +0200

    printer v1.0

--- support vector machines/printer.py
+++ support vector machines/printer.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 
 
-def plotAll(X, t):
+def plotAll(X, t, indicator):
     plotPoints(X, t)
+    plotContour(indicator)
 
-    print("Not Done Yet")
 
 def plotPoints(X, t):
     (plusArray, minusArray) = separate(X, t)
@@ -28,3 +28,15 @@ def separate(X, t):
     return (np.array(tplus), np.array(tminus))
 
 plotPoints(np.array([[1, 1], [2, 2], [2, 3]]), np.array([1, -1, 1]))
+
+def plotContour(indicator):
+    xgrid = numpy.linspace(-5, 5)
+    ygrid = numpy.linspace(-4, 4)
+
+    grid = numpy.array([[indicator((x, y)) for x in xgrid] for y in ygrid])
+
+    plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0),
+                colors=('red', 'black', 'blue'), linewidth=(1, 3, 1))
+
+
+
